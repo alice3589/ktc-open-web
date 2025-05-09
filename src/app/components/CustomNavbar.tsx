525034384b2663221a373c8570550e2f6ed2b237
alice3589 <ryutoiwa2929@gmail.com>
2025-05-09 10:46:55 +0900

    message

--- src/app/components/CustomNavbar.tsx
+++ src/app/components/CustomNavbar.tsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { ContentType } from './types';
+import Image from 'next/image';
 
 interface CustomNavbarProps {
   onSelect: (type: ContentType) => void;
@@ -8,7 +9,18 @@ interface CustomNavbarProps {
 export const CustomNavbar = ({ onSelect }: CustomNavbarProps) => (
   <Navbar bg="light" expand="lg" className="mb-4 position-fixed w-100" style={{ zIndex: 1000 }}>
     <Container>
-      <Navbar.Brand href="#home">学校ポータル</Navbar.Brand>
+      <div className="flex items-center">
+        <div className="relative w-8 h-8 mr-2">
+          <Image
+            src="/images/logo.png"
+            alt="学校ロゴ"
+            fill
+            className="object-contain"
+            sizes="32px"
+          />
+        </div>
+        <Navbar.Brand href="#home">なびげーしょんばー</Navbar.Brand>
+      </div>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
